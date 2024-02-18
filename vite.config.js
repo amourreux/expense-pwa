cbbc0160d73ffd8d34aef3e5d31ff8d8693e313a
Emre Portakal <emre.portakal@xenonsmart.com>
2024-02-18 22:17:08 +0300

    fixed issue with icons

--- vite.config.js
+++ vite.config.js
@@ -52,12 +52,6 @@ export default defineConfig({
             src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png"
-          },
-          {
-            src: "maskable-icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable any"
           }
         ]
       }
